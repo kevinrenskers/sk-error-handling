a239f8433386cc607b54fcf81f32af1ffdc02e6f
Kevin Renskers <kevin@loopwerk.io>
2022-09-09 21:33:49 +0200

    Simplify example

--- src/routes/auth/exchange-magic-token/+server.ts
+++ src/routes/auth/exchange-magic-token/+server.ts
@@ -1,9 +1,9 @@
-import { json, type RequestHandler } from "@sveltejs/kit";
-import { postApi } from "$lib/api";
-import { dev } from "$app/environment";
+import { error } from "@sveltejs/kit";
+import type { RequestHandler } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async ({ request, cookies }) => {
-  const body = await postApi<{ token: string }>(fetch, "/auth/exchange-magic-token", await request.json());
-  cookies.set("token", body.token, { path: "/", httpOnly: true, secure: !dev, maxAge: 31536000 });
-  return json({ ...body });
+  throw error(400, {
+    message: "This is not a valid login link. Has it already been used?",
+    errors: { foo: ["Just some dummy data"] },
+  });
 };
